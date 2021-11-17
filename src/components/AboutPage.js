114facebd89183e21d9b29426ae149c157f8bc38
Kenisha <k4perera@uwaterloo.ca>
2021-11-17 15:28:51 -0500

    courses page not working

---
+++ src/components/AboutPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2 style={{ margin: 10, color: "orange", background: "white" }}>
+          About
+        </h2>
+        <p style={{ margin: 10 }}>This app uses React.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
