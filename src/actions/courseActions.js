642144ee8441d963b872de5cbaaa0ba797cab191
Kenisha <k4perera@uwaterloo.ca>
2021-11-21 21:50:19 -0500

    finished webpage

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,10 +4,10 @@ import actionTypes from "./actionTypes";
 
 export function saveCourse(course) {
   return courseApi.saveCourse(course).then((savedCourse) => {
-    // Hey dispatcher, go tell all the stores that a course was just created
+    // Hey dispatcher, go tell all the stores that a course was just created.
     dispatcher.dispatch({
       actionType: course.id
-        ? actionTypes.UDPATE_COURSE
+        ? actionTypes.UPDATE_COURSE
         : actionTypes.CREATE_COURSE,
       course: savedCourse,
     });
@@ -16,10 +16,18 @@ export function saveCourse(course) {
 
 export function loadCourses() {
   return courseApi.getCourses().then((courses) => {
-    // Hey dispatcher, go tell all the stores that a course was just created
     dispatcher.dispatch({
       actionType: actionTypes.LOAD_COURSES,
       courses: courses,
+    });
+  });
+}
+
+export function deleteCourse(id) {
+  return courseApi.deleteCourse(id).then(() => {
+    dispatcher.dispatch({
+      actionType: actionTypes.DELETE_COURSE,
+      id: id,
     });
   });
 }
--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,10 @@ function App() {
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/courses" component={CoursesPage} />
+        <Route path="/courses/" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
-        <Route path="/course" component={ManageCoursePage} />
+        <Route path="/course/" component={ManageCoursePage} />
         <Redirect from="/about-page" to="about" />
         <Route component={NotFoundPage} />
       </Switch>
